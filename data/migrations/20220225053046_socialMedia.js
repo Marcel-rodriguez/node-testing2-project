6293aa40a67dd709adab28e2651d7edcb3a39d7b
marcel <marcel_rodrgez@yahoo.com>
2022-02-25 00:39:10 -0500

    made migration tables

--- data/migrations/20220225053046_socialMedia.js
+++ data/migrations/20220225053046_socialMedia.js
@@ -1,8 +1,37 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema
+  .createTable('users', table => {
+      table.increments('user_id')
+      table.string('user_name', 255).notNullable()
+  })
+  .createTable('roles', table => {
+    table.increments('role_id')
+    table.string('role_name', 255).notNullable()
+    table.integer('user_id')
+    .unsigned()
+    .notNullable()
+    .references('user_id')
+    .inTable('users')
+    .onUpdate('RESTRICT')
+    .onDelete('RESTRICT')
+  })
+  .createTable('posts', table => {
+    table.increments('post_id')
+    table.string('post_content', 255)
+    table.integer('user_id')
+    .unsigned()
+    .notNullable()
+    .references('user_id')
+    .inTable('users')
+    .onUpdate('RESTRICT')
+    .onDelete('RESTRICT')
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists('posts')
+    .dropTableIfExists('roles')
+    .dropTableIfExists('users')
 };
